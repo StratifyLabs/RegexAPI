4c99ec2ea81becef7797b64ea80603f590ebece0
Tyler <tyler-gilbert@users.noreply.github.com>
2022-08-10 21:43:44 -0500

    Initial commit

---
+++ tests/src/UnitTest.hpp
@@ -0,0 +1,22 @@
+﻿
+#include <cstdio>
+
+#include <fs.hpp>
+#include <printer.hpp>
+#include <test/Test.hpp>
+#include <var.hpp>
+
+#include "lib.hpp"
+
+class UnitTest : public test::Test {
+public:
+  UnitTest(var::StringView name) : test::Test(name) {}
+
+  bool execute_class_api_case() {
+
+
+    return true;
+  }
+
+
+};
